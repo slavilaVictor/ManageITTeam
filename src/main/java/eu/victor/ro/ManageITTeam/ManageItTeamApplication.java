6bf282c8ea06ae985aa367ec4a3e17421c5cddf1
Victor-Florin Slavila <victor.florin@yahoo.com>
2021-03-20 15:46:53 +0200

    first commit

---
+++ src/main/java/eu/victor/ro/ManageITTeam/ManageItTeamApplication.java
@@ -0,0 +1,13 @@
+package eu.victor.ro.ManageITTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageItTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageItTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/victor/ro/ManageITTeam/ManageItTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.victor.ro.ManageITTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageItTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
