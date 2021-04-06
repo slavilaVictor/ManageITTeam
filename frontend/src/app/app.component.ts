6405add28cc4a4745ec913cbb5bdf3fd47c3d254
victorSlavila <DrumuridemuntE12>
2021-04-06 16:40:43 +0300

    add, edit and delete a member

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Member } from './model/member';
 import { MemberService } from './service/member.service';
 
@@ -11,6 +12,8 @@ import { MemberService } from './service/member.service';
 export class AppComponent implements OnInit {
   title = 'frontITTeam';
   public members: Member[];
+  public editMember: Member;
+  public deleteMember: Member;
   
   constructor(private memberService: MemberService){}
 
@@ -27,6 +30,72 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  // addForm.value will be a JSON interpretation
+  public onAddMember(addForm: NgForm): void{
+    document.getElementById('add-member-form').click();
+    this.memberService.addMember(addForm.value).subscribe(
+      (response: Member) => {
+        console.log(response);
+        this.getMembers();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onEditMember(member: Member): void{
+    this.memberService.updateMember(member).subscribe(
+      (response: Member) => {
+        console.log(response);
+        this.getMembers();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteMember(memberId: number): void{
+    this.memberService.deleteMember(memberId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getMembers();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  // SEE BOOTSTRAP MODAL FOR MORE INFORMATION
+  //member-> the member that I use
+  //mode -> what I want to do with this member, in order to know which modal I open
+  public onOpenModal(member: Member, mode: string): void{
+    const container = document.getElementById('main-container');
+    //create a button programmatically; I can do the same thing in html
+    const button = document.createElement('button');
+    button.type='button';
+    // I will hide  my button in my UI, because I already have different buttons
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+    // I want to see what button was pressed
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addMemberModal');
+    }
+    if(mode === 'edit'){
+      this.editMember = member;
+      button.setAttribute('data-target', '#updateMemberModal');
+    }
+    if(mode === 'delete'){
+      this.deleteMember = member;
+      button.setAttribute('data-target', '#deleteMemberModal');
+    }
+    // In this moment, this button only exists in the ts file. I have to find a way to put it into the html
+    container.appendChild(button);
+    button.click();
   }
 
 
