6cc458827563ad60de5de481d6cd841810bcfdf5
victorSlavila <DrumuridemuntE12>
2021-04-06 12:30:00 +0300

    deal with CORS Configuration and get all the members

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Member } from './model/member';
+import { MemberService } from './service/member.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontITTeam';
+  public members: Member[];
+  
+  constructor(private memberService: MemberService){}
+
+  ngOnInit(): void {
+    this.getMembers();
+  }
+
+  public getMembers(): void{
+    this.memberService.getMembers().subscribe(
+      (response: Member[])=>{
+        this.members = response;
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    );
+  }
+
+
+
 }
--- frontend/src/app/service/member.service.ts
+++ frontend/src/app/service/member.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Member } from '../model/member';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
-  private apiServerURL = '';
+  private apiServerURL = environment.apiBaseURL;
 
   constructor(private http: HttpClient) { }
 
